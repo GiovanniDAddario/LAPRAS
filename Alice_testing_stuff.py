3c09b2924a944a76bde351d5d9d4639fda367d5c
a-p25 <40673442+a-p25@users.noreply.github.com>
2020-05-08 17:21:39 +0100

    Update Alice_testing_stuff.py

--- Alice_testing_stuff.py
+++ Alice_testing_stuff.py
@@ -22,11 +22,36 @@ class Simulation:
                             'yrs75-84': 0.0588766752,
                             'yrs85plus':0.02409275711}
 
+
+   # def create_people(self, num):
+    #    people_list = []
+       # id = 1
+     #   position = [0, 0]
+      #  possible_locations = []
+        #for n in num:
+         #   people_list.append(Person(id, position, age, vulnerability, infection_status, 1, True, possible_locations))
+
+
     def draw(self):
         fig = plt.figure()
         plt.xlim(-self.size[0],self.size[0])
         plt.ylim(-self.size[1],self.size[1])
         plt.show()
 
+
+    def initial_house_placement(self):
+        x_houses = np.random.uniform(size = int(self.population/4))*200 -100
+        y_houses = np.random.uniform(size = int(self.population/4))*200 -100
+        self.houselist = [House('House'+str(i),[x_houses[i],y_houses[i]],[10,10],4,0,2,10,self.buildinglist) for i in range(int(self.population/4))]
+      
+      	
+  
+
+    #def animateSim(self, i):
+        # Perform a single animation step
+
+    
 sim = Simulation([50, 50], 10, 100, 1)
 sim.draw()
+sim.iniitial_house_placement()
+
